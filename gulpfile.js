909ab3aaa736e77278a040ad30d8f7a139c53167
mohamed eissa <mohamed_diamondwings@yahoo.com>
2020-06-03 08:56:57 +0200

    initial commit

--- gulpfile.js
+++ gulpfile.js
@@ -21,8 +21,12 @@ gulp.task('html', function(){
 });
 
 gulp.task('css', function(){
-    return gulp.src('src/css/*.css')
-    .pipe(gulp.dest('build/css')).pipe(connect.reload());
+    var flip = gulp.src(['src/css/*.css', '!src/css/style.css']).pipe(flipper());
+    var noflip = gulp.src('src/css/style.css');
+
+    return gulp.src('src/css/*.css').pipe(flipper())
+    .pipe(gulp.dest('build/css'))
+    .pipe(connect.reload());
 });
 
 gulp.task('js', function(){
